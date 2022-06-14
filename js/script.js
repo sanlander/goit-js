5f9357424bacfce0c0b43f4a5c56cd7ea4493b3b
Alexandr Gorshkoder <S.gorshkoder@gmail.com>
2022-06-14 18:17:48 +0300

    +

--- js/script.js
+++ js/script.js
@@ -1,15 +1,23 @@
-const personal = 5;
-const minSalary = 1000;
-const maxSalary = 5500;
-let totalSalary = 0;
+const totalSpent = 800;
+let payment = 50888;
+let discount;
+let pay = payment - payment * discount;
 
-for (let i = 1; i <= personal; i += 1) {
-    let salary = Math.round(Math.random() * (maxSalary - minSalary) + minSalary);
-
-    console.log(`З/П сотрудника ${i}: ${salary}`);
-
-    totalSalary += salary;
-    console.log(`Итого зп: ${totalSalary}`);
+if (totalSpent >= 100 && totalSpent < 1000) {
+  discount = 0.02;
+  console.log(`Оформляем зкаказ на сумму ${payment} грн. со скидкой ${discount * 100}%`);
+  console.log(`Итого к оплате: ${pay} грн.`);
+  console.log(`Сумма скидки: ${(payment - (payment - payment * discount)).toFixed(2)} грн.`);
+} else if (totalSpent >= 1000 && totalSpent < 5000) {
+  discount = 0.05;
+  console.log(`Оформляем зкаказ на сумму ${payment} грн. со скидкой ${discount * 100}%`);
+  console.log(`Итого к оплате: ${payment - payment * discount} грн.`);
+  console.log(`Сумма скидки: ${payment -= payment * discount} грн.`);
+} else if (totalSpent >= 5000) {
+  discount = 0.1;
+  console.log(`Оформляем зкаказ на сумму ${payment} грн. со скидкой ${discount * 100}%`);
+  console.log(`Итого к оплате: ${(payment -= payment * discount)} грн.`);
+  console.log(`Сумма скидки: ${Math.round(payment -= payment * discount)} грн.`);
+} else {
+  console.log(`Оформляем зкаказ на сумму ${payment} грн. со скидкой ${discount * 100}%`);
 }
-
-console.log(`Общая з/п всех ${personal} сотрудников составляет: ${totalSalary} USD`); 

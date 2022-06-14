2acda3bfb444b03f9c67f4296741e4e60b469f7a
Alexandr Gorshkoder <88847067+sanlander@users.noreply.github.com>
2022-06-14 14:44:49 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,14 +1,15 @@
+const personal = 5;
+const minSalary = 1000;
+const maxSalary = 5500;
 let totalSalary = 0;
-let employees = 2;
 
-for (let i = 1; i <= employees; i += 1) {
-    let salary = Math.round(Math.random() * (5000 - 500) + 500);
-   
-    console.log(`ЗП сотрудникаа ${i} - ${salary}`);
+for (let i = 1; i <= personal; i += 1) {
+    let salary = Math.round(Math.random() * (maxSalary - minSalary) + minSalary);
+
+    console.log(`З/П сотрудника ${i}: ${salary}`);
 
     totalSalary += salary;
-    
+    console.log(`Итого зп: ${totalSalary}`);
 }
 
-
-console.log('totalSalary:', totalSalary);
+console.log(`Общая з/п всех ${personal} сотрудников составляет: ${totalSalary} USD`); 

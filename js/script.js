1af4a7ebc1a2794e76b6441adcae44788c796d19
Alexandr Gorshkoder <88847067+sanlander@users.noreply.github.com>
2022-06-15 09:40:02 +0300

    discount +

--- js/script.js
+++ js/script.js
@@ -1,23 +1,47 @@
-const totalSpent = 800;
-let payment = 50888;
-let discount;
-let pay = payment - payment * discount;
+let totalSpent = 0;
+let payment = 5001;
+let discount = 0;
 
-if (totalSpent >= 100 && totalSpent < 1000) {
+if (
+  (totalSpent >= 100 && totalSpent < 1000) ||
+  (totalSpent < 100 && payment > 5000)
+) {
   discount = 0.02;
-  console.log(`Оформляем зкаказ на сумму ${payment} грн. со скидкой ${discount * 100}%`);
-  console.log(`Итого к оплате: ${pay} грн.`);
-  console.log(`Сумма скидки: ${(payment - (payment - payment * discount)).toFixed(2)} грн.`);
+  totalSpent += payment - payment * discount;
+  console.log(
+    `Оформляем зкаказ на сумму ${payment} грн. со скидкой ${discount * 100}%`
+  );
+  console.log(`Итого к оплате: ${payment - payment * discount} грн.`);
+  console.log(
+    `Сумма скидки: ${(payment - (payment - payment * discount)).toFixed(
+      2
+    )} грн.`
+  );
 } else if (totalSpent >= 1000 && totalSpent < 5000) {
   discount = 0.05;
-  console.log(`Оформляем зкаказ на сумму ${payment} грн. со скидкой ${discount * 100}%`);
+  console.log(
+    `Оформляем зкаказ на сумму ${payment} грн. со скидкой ${discount * 100}%`
+  );
   console.log(`Итого к оплате: ${payment - payment * discount} грн.`);
-  console.log(`Сумма скидки: ${payment -= payment * discount} грн.`);
+  console.log(
+    `Сумма скидки: ${(payment - (payment - payment * discount)).toFixed(
+      2
+    )} грн.`
+  );
 } else if (totalSpent >= 5000) {
   discount = 0.1;
-  console.log(`Оформляем зкаказ на сумму ${payment} грн. со скидкой ${discount * 100}%`);
+  console.log(
+    `Оформляем зкаказ на сумму ${payment} грн. со скидкой ${discount * 100}%`
+  );
   console.log(`Итого к оплате: ${(payment -= payment * discount)} грн.`);
-  console.log(`Сумма скидки: ${Math.round(payment -= payment * discount)} грн.`);
+  console.log(
+    `Сумма скидки: ${(payment - (payment - payment * discount)).toFixed(
+      2
+    )} грн.`
+  );
 } else {
-  console.log(`Оформляем зкаказ на сумму ${payment} грн. со скидкой ${discount * 100}%`);
+  console.log(
+    `Оформляем зкаказ на сумму ${payment} грн. со скидкой ${discount * 100}%`
+  );
 }
+console.log('totalSpent:', totalSpent);
